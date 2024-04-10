54b183fe00a238ee0ac80f85c953b8fdfea42811
Semyon Saprykin <saprykinsemen@gmail.com>
2024-04-10 16:02:16 +0300

    commit

--- lab-2-2/lab-2-2.cpp
+++ lab-2-2/lab-2-2.cpp
@@ -7,9 +7,9 @@ using namespace std;
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    vector<double> arr1;
-    vector<double> arr2;
-    double number;
+    vector<double> arr1; // Вектор для хранения введённых чисел
+    vector<double> arr2; // Вектор для хранения чисел после сложения крайних
+    double number; // Переменная для хранения введённого числа
 
     // Заполнение массива arr1 вводом от пользователя
     cout << "Введите 10 чисел:" << endl;
@@ -19,29 +19,29 @@ int main() {
             cout << "Введена не цифра. Программа прервана." << endl;
             return 1; // Выход из программы с кодом ошибки
         }
-        arr1.push_back(number);
+        arr1.push_back(number); // Добавление числа в массив
     }
 
     cout << "Введённый массив: ";
     for (double num: arr1) {
-        cout << num << " ";
+        cout << num << " "; // Вывод массива на экран
     }
     cout << endl;
 
     for (int i = 0; i < arr1.size() / 2 + arr1.size() % 2; ++i) {
-        arr2.push_back(arr1[i] + arr1[arr1.size() - 1 - i]);
+        arr2.push_back(arr1[i] + arr1[arr1.size() - 1 - i]); // Сложение крайних чисел и добавление в массив arr2 
     }
 
     cout << "Полученный массив чисел, после сложения крайних: ";
     for (double num: arr2) {
-        cout << num << " ";
+        cout << num << " "; // Вывод массива на экран после сложения крайних чисел 
     }
     cout << endl;
 
     // Нахождение наибольшего числа в массиве arr2
-    double largestNumber = *max_element(arr2.begin(), arr2.end());
+    double largestNumber = *max_element(arr2.begin(), arr2.end()); // Используем функцию max_element из библиотеки algorithm
 
-    cout << "Наибольшее число: " << largestNumber << endl;
+    cout << "Наибольшее число: " << largestNumber << endl; // Вывод наибольшего числа на экран
 
     return 0;
 }
